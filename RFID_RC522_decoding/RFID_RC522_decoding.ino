3640cfbfade4a68c6645d090342e539770ffb18c
Bhupendra Singh <itsbhupendrasingh@gmail.com>
2023-01-16 16:58:22 +0530

    Update RFID_RC522_decoding.ino

--- RFID_RC522_decoding/RFID_RC522_decoding.ino
+++ RFID_RC522_decoding/RFID_RC522_decoding.ino
@@ -6,8 +6,8 @@
 #include <SPI.h> 
 #include <MFRC522.h> 
 
-byte readCard[4];     // Array that will hold UID of the RFID card.
-int successRead;
+byte ReadCard[4];     // Array that will hold UID of the RFID card.
+int ScanDone;
 
 MFRC522 mfrc522(10, 9);   // (SDA pin, RST pin)
 MFRC522::MIFARE_Key key;          // Creates MIFARE key instance.
@@ -21,10 +21,10 @@ void setup()
   Serial.println("Please Tab your Card to the RFID Reader");
 
   do {
-    successRead = scan_UID();     // Loops getID library function until reading process is done.
+    ScanDone = scan_UID();     // Loops getID library function until reading process is done.
   }
   
-  while (!successRead);
+  while (!ScanDone);
 }
 
 void loop()
@@ -47,8 +47,8 @@ int scan_UID() // Function to read RFID Card UID
   
   Serial.print("Hi, Your Card UID is: ");    
   for (int i = 0; i < mfrc522.uid.size; i++) {  
-    readCard[i] = mfrc522.uid.uidByte[i];   
-    Serial.print(readCard[i], HEX);         
+    ReadCard[i] = mfrc522.uid.uidByte[i];   
+    Serial.print(ReadCard[i], HEX);         
   }
   Serial.println("");
   mfrc522.PICC_HaltA();     // Halt the Card Scanning
